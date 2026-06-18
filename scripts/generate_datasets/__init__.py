b152a1196ed1783a7c9e82ef5290a0d80150e7d6
Ahmed Sherif <as4349124@gmail.com>
2026-06-18 12:58:23 +0300

    Adds last step which is pushing data to huggingface

--- scripts/generate_datasets/__init__.py
+++ scripts/generate_datasets/__init__.py
@@ -1,9 +1,11 @@
 from .generate_instruction_dataset import generate_intruction_dataset
 from .generate_preference_dataset import generate_preference_dataset
 from .query_feature_store import query_feature_store
+from .push_to_huggingface import push_to_huggingface
 
 __all__ = [
     "generate_intruction_dataset",
     "generate_preference_dataset",
     "query_feature_store",
+    "push_to_huggingface",
 ]
