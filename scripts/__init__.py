1d7d0117502b03f4c51c527abd134e17000aca39
Ahmed Sherif <as4349124@gmail.com>
2026-06-30 10:43:20 +0300

    Adds eval ti the init file

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,15 @@
 from . import etl
-from . import feature_engineering, generate_datasets, export_data, training
+from . import feature_engineering
+from . import generate_datasets
+from . import export_data
+from . import training
+from . import evaluating
 
-__all__ = ["etl", "feature_engineering", "generate_datasets", "export_data", "training"]
+__all__ = [
+    "etl",
+    "feature_engineering",
+    "generate_datasets",
+    "export_data",
+    "training",
+    "evaluating",
+]
